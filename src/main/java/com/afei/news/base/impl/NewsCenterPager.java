4f87abc25705df0fd6b2a62229cd04453544d4fb
fei <719584671@qq.com>
2016-12-22 00:07:50 +0800

    从服务器获取数据并用gson解析

--- src/main/java/com/afei/news/base/impl/NewsCenterPager.java
+++ src/main/java/com/afei/news/base/impl/NewsCenterPager.java
@@ -1,9 +1,19 @@
 package com.afei.news.base.impl;
 
 import android.app.Activity;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.afei.news.base.BasePager;
+import com.afei.news.domain.NewsMenuData;
+import com.afei.news.global.Constants;
+import com.google.gson.Gson;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
 
 /**
  * 新闻中心
@@ -21,5 +31,49 @@ public class NewsCenterPager extends BasePager {
         TextView textView = new TextView(mActivity);
         textView.setText("新闻中心");
         flContent.addView(textView);
+
+        getDataFromServer();
+    }
+
+    /**
+     * 从服务器获取数据
+     */
+    private void getDataFromServer() {
+        HttpUtils httpUtils = new HttpUtils();
+        httpUtils.send(HttpRequest.HttpMethod.GET, Constants.CATEGORIES_URL, new RequestCallBack<String>() {
+
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                String result = responseInfo.result;//获取json字符串
+                Log.d("NewsCenterPager",result);
+                processResult(result);
+
+//                Log.d("NewsCenterPager","onsuccess");
+            }
+
+            @Override
+            public void onFailure(HttpException error, String msg) {
+                Log.d("NewsCenterPager",msg);
+                error.printStackTrace();
+                Toast.makeText(mActivity,msg,Toast.LENGTH_SHORT);
+
+            }
+        });
+
+
+
+    }
+
+    /**
+     * 解析从服务器拿到的数据：gson解析
+     */
+    private void processResult(String result) {
+        Log.d("haha","111111111");
+        Gson gSon = new Gson();
+        Log.d("haha","222222222222");
+        NewsMenuData newsMenuData = gSon.fromJson(result, NewsMenuData.class);
+        Log.d("haha","3333333333");
+       String data =  newsMenuData.getData().toString();
+        Log.d("haha","44444444");
     }
 }
---
+++ src/main/java/com/afei/news/global/Constants.java
@@ -0,0 +1,10 @@
+package com.afei.news.global;
+
+/**
+ * Created by fei on 2016/12/21.
+ */
+
+public class Constants {
+    public static final String SERVER_URL = "http://192.168.56.1:8080";
+    public static final String CATEGORIES_URL = SERVER_URL+"/zhbj/categories.json";
+}
